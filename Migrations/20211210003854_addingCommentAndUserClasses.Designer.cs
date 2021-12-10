5d05eea64c0fe9d958fc86129aa93276da0fe3cb
adnanjoraid <adnanjoraidca@gmail.com>
2021-12-09 19:39:33 -0500

    added migration for updated user and comment classes

--- Migrations/20211210003854_addingCommentAndUserClasses.Designer.cs
+++ Migrations/20211210003854_addingCommentAndUserClasses.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using hashmemes.Persistence;
 
 namespace hashmemes.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20211210003854_addingCommentAndUserClasses")]
+    partial class addingCommentAndUserClasses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
