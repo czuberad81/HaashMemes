62e0e2cded126342ce757e006815360ac0d20453
adnanjoraid <adnanjoraidca@gmail.com>
2021-12-12 16:57:19 -0500

    edited group class and added http functions for group controller

--- Controllers/GroupsController.cs
+++ Controllers/GroupsController.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using hashmemes.Models;
 using hashmemes.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 /*
@@ -27,6 +29,55 @@ namespace hashmemes.Controllers
         public GroupsController(DataContext context)
         {
             _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGroup([FromBody] Group groupDetail)
+        {
+            /*
+            Summary: Method receives groupDetail which contains group name and creator (currentMember)
+            Arguments: Group object
+            Return: Http Request
+            */
+            var doesGroupExist = _context.Groups.Where(x => x.GroupName.Equals(groupDetail.GroupName));
+
+            if (doesGroupExist != null)
+            {
+                return BadRequest("Group already exist");
+            }
+
+            if (groupDetail.GroupName.Equals(null))
+            {
+                return BadRequest("Group name should not be null");
+            }
+
+            await _context.Groups.AddAsync(groupDetail);
+
+
+            return Ok($"Group Created. Group Owner is {groupDetail.CurrentOwner}");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGroupContent([FromBody] string groupId)
+        {
+            /*
+            Summary: Method receives group id to return the content
+            Arguments: Group ID
+            Return: Http Request with List<Posts> from Group class
+            */
+            if (groupId == null)
+            {
+                return BadRequest("Group ID cannot be null");
+            }
+
+            var currentPosts = _context.Groups.Include(x => x.Posts).Where(x => x.Id.Equals(new Guid(groupId)));
+            if (currentPosts == null)
+            {
+                return BadRequest("No availabe posts");
+            }
+
+
+            return Ok(currentPosts);
         }
 
 
