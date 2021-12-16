af1069d00213cb5164a669555327a50e106b3d56
adnanjoraid <adnanjoraidca@gmail.com>
2021-12-16 18:59:24 -0500

    migration

---
+++ Migrations/20211216235822_updatePostTabl.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hashmemes.Migrations
+{
+    public partial class updatePostTabl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
